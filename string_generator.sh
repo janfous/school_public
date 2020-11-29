7c53ea28c0ae19a97d6b77f8a7270c0fa8d9c48f
Jan Fous <53558137+janfous@users.noreply.github.com>
2020-11-29 13:49:09 +0100

    Update string_generator.sh

--- string_generator.sh
+++ string_generator.sh
@@ -3,7 +3,7 @@
 #in: stgen aAbBcC0123 10; out: C2ccc032Ba
 #in: stgen $(printf "%s" {a..z}{A..Z}{0..9}) 100; out: 3oQQHoiVZLZd35y224PJQhc89xY4807AFIl30BkA83ih4sV53aSao8PZ8QIb0TxLW8p2Yd79pSZI8pcv421kvB6F018P4F7hIcY9
 
-#progtest 6 phonebook input gen: stgen +-? 1; printf " "; stgen $(printf "%s" {0..9}) $((RANDOM % 300)); printf ' "'; stgen $(printf "%s" {a..z}{A..Z}{0..9}) $((RANDOM % 300)); printf '"\n'
+#progtest 6 phonebook input gen: A=$(stgen +-?); B=$(stgen $(printf "%s" {0..9}) $((RANDOM % 300))); C=$(stgen $(printf "%s" {a..z}{A..Z}{0..9}) $((RANDOM % 300))); printf '%s %s "%s"\n' $A $B $C
 
 chars="$1";
 len=$2;
