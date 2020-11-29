eba01438f07f3fa597423926851289a15b8bd1d7
Jan Fous <53558137+janfous@users.noreply.github.com>
2020-11-29 13:36:53 +0100

    Update string_generator.sh

--- string_generator.sh
+++ string_generator.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#./string_generator.sh allowed_characters length
 #usage examples: (aliased as stgen) 
 #in: stgen aAbBcC0123 10; out: C2ccc032Ba
 #in: stgen $(printf "%s" {a..z}{A..Z}{0..9}) 100; out: 3oQQHoiVZLZd35y224PJQhc89xY4807AFIl30BkA83ih4sV53aSao8PZ8QIb0TxLW8p2Yd79pSZI8pcv421kvB6F018P4F7hIcY9
+
+#progtest 6 phonebook input gen: stgen +-? 1; printf " "; stgen $(printf "%s" {0..9}) $((RANDOM % 300)); printf " "; stgen $(printf "%s" {a..z}{A..Z}{0..9}) $((RANDOM % 300)) 
 
 chars="$1";
 len=$2;
 for i in $(seq 1 $len); do
     echo -n "${chars:RANDOM%${#chars}:1}"
 done
-echo
+#echo < uncomment to end output with newline
